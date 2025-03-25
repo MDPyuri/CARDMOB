7973710e3b133da41a7dd92da1ecce5f753e0d64
MDPyuri <matsumoto.prado@aluno.ifsp.edu.br>
2025-03-25 08:56:44 -0300

    Add TodoList componente

--- Etapa1/react-exemplos/src/App.jsx
+++ Etapa1/react-exemplos/src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 import Counter from './components/Counter'
 import Photo from './components/Photo'
 import Album from './components/Album'
+import TodoList from './components/TodoList';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -43,6 +44,7 @@ function App() {
 
   return (
     <>
+      <TodoList name="CARDMOB"/>
       <Counter title="Contador Superior"/>
       <Counter initial="77"/>
 
