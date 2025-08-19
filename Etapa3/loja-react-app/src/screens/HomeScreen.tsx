871729bed1d5dbfcf0b8f104aaf264cca5699336
MDPyuri <matsumoto.prado@aluno.ifsp.edu.br>
2025-08-19 08:49:13 -0300

    Inclui novas telas: registro e login

--- Etapa3/loja-react-app/src/screens/HomeScreen.tsx
+++ Etapa3/loja-react-app/src/screens/HomeScreen.tsx
@@ -1,22 +1,35 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
-
 import { useTheme } from "../contexts/ThemeContext";
 
-function HomeScreen() {
+function HomeScreen({ navigation}: any) {
   const { theme, toggleTheme } = useTheme();
   return (
-    <View style={[styles.container, { backgroundColor: theme.colors.background }]}>
-
-      <Text style= {{ color: theme.colors.text, marginBottom: theme.spacing(1) }}>
-        Home Screen
-      </Text>
-      <Button
-        title="Change Mode"
-        onPress={toggleTheme}
-        color={theme.colors.primary}
-      />
-    </View>
+      <View
+          style={[
+              styles.container,
+              { backgroundColor: theme.colors.background },
+          ]}
+      >
+          <Text
+              style={{
+                  color: theme.colors.text,
+                  marginBottom: theme.spacing(1),
+              }}
+          >
+              Home Screen
+          </Text>
+          <Button
+              title="Alternar Tema"
+              onPress={toggleTheme}
+              color={theme.colors.primary}
+          />
+          <Button
+              title="Ir para Detalhes"
+              onPress={() => navigation.navigate('Details')}
+          />
+          <Button title="Login" onPress={() => navigation.navigate('Login')} />
+      </View>
   );
 }
 export default HomeScreen;
