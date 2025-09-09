242f527f952705daca417dc5372db635d68dcc9b
MDPyuri <matsumoto.prado@aluno.ifsp.edu.br>
2025-09-09 10:19:09 -0300

    Inclui catalogService.ts

---
+++ Etapa3/loja-react-app/src/services/catalogService.tsx
@@ -0,0 +1,13 @@
+const API_URL = 'http://10.81.205.27:5000';
+
+export async function getCatalog(): Promise<any> {
+    try {
+        const response = await fetch(`${API_URL}/catalog`);
+        const data = await response.json();
+        console.log(data);
+        return Promise.resolve(data.catalog);
+    } catch (error) {
+        console.error(error);
+        return Promise.reject('Erro ao obter produtos');
+    }
+}
